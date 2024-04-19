ec123191a425b465b3031cdcd6c88a1a65c41027
lsilva56 <lsilva56@toromail.csudh.edu>
2024-04-19 01:39:18 -0700

    updated TrackedWorkoutsScreen.js to sort by newest

--- TrackedWorkoutsScreen.js
+++ TrackedWorkoutsScreen.js
@@ -1,8 +1,7 @@
-// TrackedWorkoutsScreen.js
 import React, { useState, useEffect } from 'react';
 import { View, Text, FlatList, StyleSheet } from 'react-native';
 import { db } from './FirebaseConfig'; // Import your db instance from the firebase config file
-import { collection, getDocs } from 'firebase/firestore';
+import { collection, query, orderBy, getDocs } from 'firebase/firestore'; // Import query and orderBy
 
 const TrackedWorkoutsScreen = () => {
   const [trackedWorkouts, setTrackedWorkouts] = useState([]);
@@ -10,8 +9,9 @@ const TrackedWorkoutsScreen = () => {
   useEffect(() => {
     const fetchWorkouts = async () => {
       const workoutsCollectionRef = collection(db, 'workouts'); // Reference to your Firestore collection
+      const workoutsQuery = query(workoutsCollectionRef, orderBy('timestamp', 'desc')); // Query that orders the workouts by the 'createdAt' field, newest first
       try {
-        const querySnapshot = await getDocs(workoutsCollectionRef);
+        const querySnapshot = await getDocs(workoutsQuery);
         const workouts = querySnapshot.docs.map(doc => {
           const data = doc.data();
           return {
@@ -38,7 +38,6 @@ const TrackedWorkoutsScreen = () => {
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => (
           <View style={styles.workoutContainer}>
-            {/* Correctly reference the nested duration object */}
             <Text style={styles.workoutText}>{`Exercise: ${item.workoutType}, Miles: ${item.miles}, Duration: ${item.duration.hours}h ${item.duration.minutes}m`}</Text>
           </View>
         )}
